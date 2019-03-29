f9b8ece0f498ab615587cb8733e365941519bc10
Nasrullah <nasrullah.syed@silbury.com>
2019-03-29 16:45:57 +0530

    added test suites

---
+++ Test Suites/menu_test_suite.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>all menus test suite</description>
+   <name>menu_test_suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c9664a5f-8b76-4f20-858d-6e08d1ac1292</testSuiteGuid>
+   <testCaseLink>
+      <guid>c29167f3-b6c4-4188-8f2e-67dac0762a83</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/products</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1b7cb213-4848-47e6-9372-c8c34e59c01b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Services</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>83bcb8db-4481-4d48-8084-eb5ff0f33a0a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/pc_acadamy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bc618140-9cbd-420a-86c4-313aba6c3749</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
