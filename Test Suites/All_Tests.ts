667489cbff00bae4beb181b8af9e6168cf8876fd
Nasrullah <nasrullah.syed@silbury.com>
2019-04-02 15:29:25 +0530

    add a new test suit

---
+++ Test Suites/All_Tests.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>All_Tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d8947b6f-47e3-4020-b32b-70253f2bbb7a</testSuiteGuid>
+   <testCaseLink>
+      <guid>c29a11a3-6fa6-4796-b34b-423bb13c1d7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/header</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a4005180-b062-4bdd-8399-595f3c7505a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/footer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5853d20d-eacf-49e4-b9fa-c8c583d9495a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/pc_acadamy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e2fa779b-e13a-4b76-b47b-82f8af85bfec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c145fcff-cea0-4da3-b553-ab023af151e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/products</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43649185-7e74-4645-964f-4ce27562fdc4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7c953ab1-860f-4d66-a1c9-5d17b107689f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Services</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
