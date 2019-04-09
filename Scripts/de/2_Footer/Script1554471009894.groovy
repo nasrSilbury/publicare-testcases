aaf3cbf03495ba432e09c415cf67c6b25c988ba1
Nasrullah <nasrullah.syed@silbury.com>
2019-04-09 13:42:47 +0530

    added delay

--- Scripts/de/2_Footer/Script1554471009894.groovy
+++ Scripts/de/2_Footer/Script1554471009894.groovy
@@ -24,6 +24,7 @@ WebUI.click(findTestObject('Object Repository/de/2_Footer/Page_HOME  Publicare/a
 //WebUI.click(findTestObject('Object Repository/de/2_Footer/Page_HOME  Publicare/a_Kontakt'))
 'scroll to bottom of page'
 WebUI.scrollToPosition(9999999, 9999999)
+WebUI.delay(3)
 
 WebUI.verifyElementVisible(findTestObject('Object Repository/de/2_Footer/Page_HOME  Publicare/publicare advise'))
 
